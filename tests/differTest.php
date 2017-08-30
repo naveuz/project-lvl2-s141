3edaddbe1f06d70f479be293348baea4cc5d5b4e
naveuz <az.gost@gmail.com>
2017-08-30 19:54:39 +0300

    change differTest & add fixtures

--- tests/differTest.php
+++ tests/differTest.php
@@ -3,34 +3,32 @@
 namespace Gendiff\Tests;
 
 use \PHPUnit\Framework\TestCase;
-use function Gendiff\differ\getDiffArray;
+use function Gendiff\differ\genDiff;
 
 class DifferTest extends TestCase
 {
     /**
      * @dataProvider additionProvider
      */
-    public function testGetDiffArray($expected, $before, $after)
+    public function testGenDiff($expected, $format, $pathToFile1, $pathToFile2)
     {
-        $this->assertEquals($expected, getDiffArray($before, $after));
+        $this->assertEquals($expected, genDiff($format, $pathToFile1, $pathToFile2));
     }
     public function additionProvider()
     {
         return [
-            [["  host" => "hexlet.io",
-              "+ timeout" => 20,
-              "- timeout" => 50,
-              "- proxy" => "123.234.53.22",
-              "+ verbose" => true
-            ],
-            ["host" => "hexlet.io",
-             "timeout" => 50,
-             "proxy" => "123.234.53.22"
-            ],
-            ["timeout" => 20,
-             "verbose" => true,
-             "host" => "hexlet.io"
-            ]]
+            [
+'{
+   host: hexlet.io
+ + timeout: 20
+ - timeout: 50
+ - proxy: 123.234.53.22
+ + port: 3306
+ + prot: http
+}',
+            'pretty',
+            'tests/fixtures/before.json',
+            'tests/fixtures/after.json']
         ];
     }
 }
