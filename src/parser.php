2f8f548698544a4fe15e989c7482c4306bf5cf38
naveuz <az.gost@gmail.com>
2017-08-31 09:38:17 +0300

    refactor parser

--- src/parser.php
+++ src/parser.php
@@ -3,13 +3,28 @@
 namespace Gendiff\parser;
 
 use Symfony\Component\Yaml\Yaml;
+use function Gendiff\lib\getContent;
+
+function parseFile($pathToFile)
+{
+    $fileInfo = new \SplFileInfo($pathToFile);
+    $extension = $fileInfo->getExtension();
+
+    switch ($extension) {
+        case 'json':
+            return parseJson($pathToFile);
+
+        case 'yml':
+            return parseYaml($pathToFile);
+    }
+}
 
 function parseJson($filePath)
 {
-    return json_decode(file_get_contents($filePath), true);
+    return json_decode(getContent($filePath), true);
 }
 
 function parseYaml($filePath)
 {
-    return Yaml::parse(file_get_contents($filePath));
+    return Yaml::parse(getContent($filePath));
 }
