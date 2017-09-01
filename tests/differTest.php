ce78c9b3e5b594e8b4731ea19cfba597f4c79058
naveuz <az.gost@gmail.com>
2017-09-01 20:21:09 +0300

    add test pretty format

--- tests/differTest.php
+++ tests/differTest.php
@@ -7,6 +7,33 @@ use function Gendiff\differ\genDiff;
 
 class DifferTest extends TestCase
 {
+    const PRETTY = <<<DOC
+
+   "common": {
+     "setting1": "Value 1"
+    -"setting2": "200"
+     "setting3": "1"
+    -"setting6": {
+       "key": "value"
+     }
+    +"setting4": "blah blah"
+    +"setting5": {
+       "key5": "value5"
+     }
+   }
+   "group1": {
+    -"baz": "bas"
+    +"baz": "bars"
+     "foo": "bar"
+   }
+  -"group2": {
+     "abc": "12345"
+   }
+  +"group3": {
+     "fee": "100500"
+   }
+
+DOC;
     /**
      * @dataProvider additionProvider
      */
@@ -16,26 +43,28 @@ class DifferTest extends TestCase
     }
     public function additionProvider()
     {
-        return [['{'.PHP_EOL.'   host: hexlet.io'.PHP_EOL.
-                 ' + timeout: 20'.PHP_EOL.
-                 ' - timeout: 50'.PHP_EOL.
-                 ' - proxy: 123.234.53.22'.PHP_EOL.
-                 ' + port: 3306'.PHP_EOL.
-                 ' + prot: http'.PHP_EOL.
-                '}',
+        return [[PHP_EOL.'   "host": "hexlet.io"'.PHP_EOL.
+                 '  -"timeout": "50"'.PHP_EOL.
+                 '  +"timeout": "20"'.PHP_EOL.
+                 '  -"proxy": "123.234.53.22"'.PHP_EOL.
+                 '  +"port": "3306"'.PHP_EOL.
+                 '  +"prot": "http"'.PHP_EOL,
                 'pretty',
                 'tests/fixtures/before.json',
                 'tests/fixtures/after.json'],
-                ['{'.PHP_EOL.'   host: hexlet.io'.PHP_EOL.
-                 ' + timeout: 20'.PHP_EOL.
-                 ' - timeout: 50'.PHP_EOL.
-                 ' - proxy: 123.234.53.22'.PHP_EOL.
-                 ' + port: 3306'.PHP_EOL.
-                 ' + prot: http'.PHP_EOL.
-                '}',
+                [PHP_EOL.'   "host": "hexlet.io"'.PHP_EOL.
+                 '  -"timeout": "50"'.PHP_EOL.
+                 '  +"timeout": "20"'.PHP_EOL.
+                 '  -"proxy": "123.234.53.22"'.PHP_EOL.
+                 '  +"port": "3306"'.PHP_EOL.
+                 '  +"prot": "http"'.PHP_EOL,
                 'pretty',
                 'tests/fixtures/before.yml',
-                'tests/fixtures/after.yml']
+                'tests/fixtures/after.yml'],
+                [self::PRETTY,
+                'pretty',
+                'tests/fixtures/before-recur.json',
+                'tests/fixtures/after-recur.json']
         ];
     }
 }
